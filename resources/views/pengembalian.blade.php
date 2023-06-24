50a90a395cbf601857faff839fd795c81ce842e1
Arxyma <ilham.arsyam@gmail.com>
2023-06-24 12:09:34 +0700

    perbaiki migrations dan pengembalian.views

--- resources/views/pengembalian.blade.php
+++ resources/views/pengembalian.blade.php
@@ -10,11 +10,13 @@
         <div class="card">
             <div class="card-header">
                 <h3>Data Pengembalian</h3>
+                @can('admin')
                 <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                     <a href="/pengembalian/tambah-pengembalian" class="btn btn-info"> <i class="bi bi-plus-circle-fill"></i> Data Pengembalian</a>
                     {{-- <a href="/kaset/cetakpdf" target="blank" class="btn btn-warning my-3 ml-2"><i class="bi bi-printer-fill"></i> Cetak PDF</a>
                     <a href="/kaset/export_excel" target="blank" class="btn btn-success my-3 ml-2"><i class="bi bi-filetype-xlsx"></i> Export Excel</a> --}}
                 </div>
+                @endcan
             </div>
             <div class="card-body">
                 <table id="pengembalian" class="table table-hover table-striped" data-export-title="Data Pengembalian">
