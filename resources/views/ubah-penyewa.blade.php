5bb0a25dd0c9783152104d96271541e3e4d434d1
Arxyma <ilham.arsyam@gmail.com>
2023-06-27 10:49:49 +0700

    fix halaman ubah penyewa

--- resources/views/ubah-penyewa.blade.php
+++ resources/views/ubah-penyewa.blade.php
@@ -12,45 +12,39 @@
     
     <div class="mx-5 px-5 card pt-5 pb-5">
             <div class="d-grid gap-2 d-md-block mb-3">
-                <a href="/kaset" class="btn btn-sm btn-warning"><i class="bi bi-arrow-left-circle-fill"></i> Kembali</a>
+                <a href="/penyewa" class="btn btn-sm btn-warning"><i class="bi bi-arrow-left-circle-fill"></i> Kembali</a>
             </div>
     
-            <h4><strong>Halaman Ubah Data Kaset </strong></h4>
+            <h4><strong>Halaman Ubah Data Penyewa </strong></h4>
     
-        <form action="/kaset/edit" method="post">
+        <form action="/penyewa/edit" method="post">
             {{ csrf_field() }}
             
             <div class="mb-3">
-                <label for="id_kaset" class="form-label">ID Kaset</label>
-                <input type="text" class="form-control" id="id_kaset" name="id_kaset" aria-describedby="id_kaset" required value="{{$d->id_kaset}}" readonly>
+                <label for="id_penyewa" class="form-label">ID Penyewa</label>
+                <input type="text" class="form-control" id="id_penyewa" name="id_penyewa" aria-describedby="id_penyewa" required value="{{$d->id_penyewa}}" readonly>
             </div>
             <div class="mb-3">
-                <label for="judul" class="form-label">Judul</label>
-                <input type="text" class="form-control" id="judul" name="judul" aria-describedby="judul" value="{{$d->judul}}" autofocus>
+                <label for="nama_penyewa" class="form-label">Nama Penyewa</label>
+                <input type="text" class="form-control" id="nama_penyewa" name="nama_penyewa" aria-describedby="nama_penyewa" value="{{$d->nama_penyewa}}" autofocus>
             </div>
             <div class="mb-3">
-                <label for="deskripsi" class="form-label">Deskripsi</label>
-                <textarea class="form-control" id="deskripsi" name="deskripsi" rows="3" required>{{$d->deskripsi}}</textarea>
-            </div>
-            <div class="mb-3">
-                <label for="tanggal_rilis" class="form-label">Tanggal Rilis</label>
-                <input type="date" class="form-control" id="tanggal_rilis" name="tanggal_rilis" aria-describedby="tanggal_rilis" required value="{{$d->tanggal_rilis}}">
+                <label for="no_ktp" class="form-label">No. KTP</label>
+                <input type="number" class="form-control" id="no_ktp" name="no_ktp" aria-describedby="no_ktp" required value="{{$d->no_ktp}}">
             </div>
 
             <div class="mb-3">
-                <label for="jumlah_stok" class="form-label">Jumlah Stok</label>
-                <input type="number" class="form-control" id="jumlah_stok" name="jumlah_stok" aria-describedby="jumlah_stok" required value="{{$d->jumlah_stok}}">
+                <label for="alamat" class="form-label">Alamat</label>
+                <textarea class="form-control" id="alamat" name="alamat" rows="3" aria-describedby="alamat" required>{{$d->alamat}}</textarea>
             </div>
-            
-            {{-- <div id="js-format-input"></div> --}}
 
             <div class="mb-3">
-                <label for="harga_sewa" class="form-label">Harga Sewa</label>
-                <input type="number" onkeyup="harga(this)" class="form-control" id="harga_sewa" name="harga_sewa" aria-describedby="harga_sewa" required value="{{$d->harga_sewa}}">
+                <label for="no_hp" class="form-label">No. HP</label>
+                <input type="number" class="form-control" id="no_hp" name="no_hp" aria-describedby="no_hp" required value="{{$d->no_hp}}">
             </div>
 
             <div class="d-grid gap-2 d-md-flex justify-content-md-end pt-3">
-            <input type="submit" class="btn btn-success" value="Ubah Kaset" >
+            <input type="submit" class="btn btn-success" value="Ubah Penyewa" >
             </div>
         </form>
         </div>
